66cb3f28b00541388ba0628b38b9c10d6770c7e0
Shirika <107738723+Ayano-Keiko@users.noreply.github.com>
2025-01-19 11:40:03 -0600

    source code upload

---
+++ BingoDialog.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace BingoGame;
+
+public partial class BingoDialog : Window
+{
+    private Image? bingoImage;
+    public BingoDialog(int imgIndex)
+    {
+        InitializeComponent();
+
+        bingoImage = (Image) FindName("BingoImage");
+
+        if (bingoImage != null)
+        {
+            // Set the Image Control
+            String fileName = $"resources/Image/Bingo/{imgIndex}.png";
+            BitmapImage bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.UriSource = new Uri(fileName, UriKind.Relative);
+            bitmapImage.EndInit();
+            bingoImage.Stretch = Stretch.Fill;
+            bingoImage.Source = bitmapImage;
+        }
+    }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        this.Close();
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace BingoGame;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private List<int> bingoNumbers;
+    private Random random;
+    
+    public MainWindow()
+    {
+        InitializeComponent();
+        
+        // fill 0 - 34 to list - bingo item lists
+        IEnumerable<int> range = Enumerable.Range(0, 35);
+        bingoNumbers = range.ToList();
+        
+        // Init random class
+        random = new Random();
+    }
+
+    private void bingo_turn(object sender, RoutedEventArgs e)
+    {
+        
+        if (bingoNumbers.Count < 2)
+        {
+            // less than 2, quit
+            // avoid out of range
+            MessageBox.Show("No Enough Value!");
+            return;
+        }
+        
+        // 1. generate random number - index
+        int currentIndex = random.Next(0, bingoNumbers.Count);
+        // 2. get the actual value
+        int currentValue = bingoNumbers[currentIndex];
+        
+        // 3. display current Bingo item
+        /* display the image displaying current item
+         */
+
+        BingoDialog bingoDialog = new BingoDialog(currentValue);
+        
+        System.Media.SystemSounds.Hand.Play();
+        bingoDialog.ShowDialog();
+        
+        /* add stroke/ mask on image
+         * set the opacity to semi-transparent to identify be used
+         */
+        String canvasName = $"Pic{currentValue}";
+        Canvas canvas = (Canvas) this.FindName(canvasName);
+
+        if (canvas != null)
+        {
+            // set the opacity mask to identify the selected item
+            canvas.Opacity = 0.2;
+        }
+        
+        // 4. delete the number in Array
+        bingoNumbers.RemoveAt(currentIndex);
+        
+        // display item index and remain size for debug - Debug only
+        // MessageBox.Show($"Now we are at {currentValue}\nand index is {currentIndex}\nand we have {bingoNumbers.Count} left");
+        
+    }
+
+    ~MainWindow()
+    {
+    }
+}
